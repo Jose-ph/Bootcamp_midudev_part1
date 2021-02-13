9e2b535d15ae188bd33af061d574e2d9ed7333be
Jose <jose_esteban_basualdo@hotmail.com>
2021-02-12 23:47:20 -0300

    update app.js

--- src/App.js
+++ src/App.js
@@ -1,10 +1,28 @@
 
 import './App.css';
 
-function App() {
+const  App = ()=> {
+
+  const mensaje = "Hola Mundo";
+  const a = 3;
+  const b = 18;
+
+
+
   return (
     <div className="App">
-      HOLA MANOLA 2
+
+      <h1>Titulo de la app</h1>
+
+      Hola manola <br/>
+
+      {mensaje + ' 2021'} <br/>
+
+      <strong>Estamos trabajando en ello...</strong>
+
+      <div>
+        <p>El resultado es : {a+b} </p>
+      </div>
      
     </div>
   );
