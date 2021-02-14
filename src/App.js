f256731c32070f44608fa5d90a2b0b53780c5bd3
Jose <jose_esteban_basualdo@hotmail.com>
2021-02-14 17:25:01 -0300

    agrega tarea part1

--- src/App.js
+++ src/App.js
@@ -1,29 +1,5 @@
 
 import './App.css';
-import './Mensaje_module.js'
-import MENSAJE_MODULO from './Mensaje_module.js';
-
-
-const Mensaje = (props)=> {
-
- //console.log(props)
-  return(
-
-    <h1 style={{color:props.color}}  >   {props.message}</h1>
-
-  )
-}
-
-const Description = () => {
-
-  return(
-
-    <p>Hola manola soy una descripción hecha desde otro componente</p>
-  )
-}
-
-
-
 
 
 const  App = ()=> {
@@ -35,15 +11,6 @@ const  App = ()=> {
   return (
     <div className="App">
 
-      <h1>Hola Manola</h1>
-           
-      <Mensaje color ="red"  message ="Estamos en un curso" />
-      <Mensaje color="green"  message ="de desarollo web Full Stack" />
-      <Mensaje color ="blue" message="con React" />
-
-      < Description/>
-
-      <MENSAJE_MODULO/>
       
       
      
