ab21363ae640697d41dad14f91a57a1a10c12e6f
Jose <jose_esteban_basualdo@hotmail.com>
2021-02-14 23:05:43 -0300

    vuelve al estado inicial

--- src/index.js
+++ src/index.js
@@ -1,57 +1,27 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 
-const Title = (props) =>{
-
-  return (<h1>{props.course}</h1>)
-
+const Header = (props) => {
 
 }
 
-const ParagraphOne = (props) => {
-
-  return (
-
-    <p>
-    {props.part1} {props.exercises1}
-  </p>
-  )
+const Content = (props) => {
 
 }
 
-const ParagraphTwo = (props) => {
+const Total = (props) =>{
 
-  return (
-
-    <p>
-    {props.part2} {props.exercises2}
-  </p>
-
-
-  )
-
-}
-
-const ParagraphThree = (props) => {
-
-  return (
-
-    <p>
-    {props.part3} {props.exercises3}
-  </p>
-  )
-}
-
-const ParagraphFour = (props) => {
-
-  return (
-
-    <p> Number of exercises {props.exercises1 + props.exercises2 + props.exercises3}</p>
+  
+} 
 
 
 
-  )
-}
+
+
+
+
+
+
 
 const App = () => {
   const course = 'Half Stack application development'
@@ -64,19 +34,18 @@ const App = () => {
 
   return (
     <div>
-      <Title course = {course} />
-      
-      < ParagraphOne part1={part1} exercises1 = {exercises1} />
-      
-      <ParagraphTwo part2= {part2} exercises2= {exercises2} />
-
-      <ParagraphThree part3 = {part3} exercises3 = {exercises3} />
-
-      <ParagraphFour exercises1= {exercises1} exercises2 = {exercises2} exercises3 = {exercises3} />
-      
-      
+      <h1>{course}</h1>
+      <p>
+        {part1} {exercises1}
+      </p>
+      <p>
+        {part2} {exercises2}
+      </p>
+      <p>
+        {part3} {exercises3}
+      </p>
+      <p>Number of exercises {exercises1 + exercises2 + exercises3}</p>
     </div>
-
   )
 }
 
