47ef1b2484e1213fcd015b1225b619a2aa80158c
Jose <jose_esteban_basualdo@hotmail.com>
2021-02-15 12:27:07 -0300

    agrega set interval

--- src/index.js
+++ src/index.js
@@ -1,94 +1,19 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
-
-const Header = (props) => {
-
-  
-
-  return (
-   
-    <h1 style = {{color: props.color}}  >{props.course}</h1>
-  )
-
-}
-
-
-const Content = (props) => {
-
-  return (
-    <>
-      
-
-   </>
-  )
-
-}
-
-const Total = (props) =>{
-
-  const part1 = {
-    name: 'Fundamentals of React',
-    exercises: 10
-  }
-  const part2 = {
-    name: 'Using props to pass data',
-    exercises: 7
-  }
-
-  const part3 = {
-    name: 'State of a component',
-    exercises: 14
-  }
-
-
-  return  (
-
-    <p>Number of exercises {part1.exercises + part2.exercises + part3.exercises}</p>
-  )
-
-
-} 
+import {useState} from 'react'
 
 
 
+const App = (props) => {
 
 
-
-
-
-
-
-const App = () => {
-
-  const course = {
-    name: 'Half Stack application development',
-    parts: [
-      {
-        name: 'Fundamentals of React',
-        exercises: 10
-      },
-      {
-        name: 'Using props to pass data',
-        exercises: 7
-      },
-      {
-        name: 'State of a component',
-        exercises: 14
-      }
-    ]
-  }
+  let contador = 0
   
 
   return (
     <div>
 
-      <Header color = 'red' course = {course.name} />
-
-      <p>{course.parts[0].name} {course.parts[0].exercises}</p>
-      <p>{course.parts[1].name} {course.parts[1].exercises}</p>
-      <p>{course.parts[2].name} {course.parts[2].exercises}</p>
-
-      <p>Number of exercises {course.parts[0].exercises + course.parts[1].exercises + course.parts[2].exercises}</p>
+     <h1> El contador está en {contador} </h1>
       
      
       
@@ -96,4 +21,14 @@ const App = () => {
   )
 }
 
-ReactDOM.render(<App />, document.getElementById('root'))
+
+
+
+
+  ReactDOM.render(<App/>,
+    document.getElementById('root'))
+ 
+
+
+
+
